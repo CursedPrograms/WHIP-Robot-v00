0c84674981323c17163661bbcc3ac16367540d35
Cursed Entertainment <110344485+CursedPrograms@users.noreply.github.com>
2026-04-14 22:09:37 +0200

    ...

--- setup.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-python -m venv psdenv
-cmd /k ".\psdenv\Scripts\activate & python main.py"
--- setup.ps1
+++
@@ -1,6 +0,0 @@
-# Create a Python virtual environment
-python -m venv psdenv
-
-# Activate the virtual environment and run the Python script
-& ".\psdenv\Scripts\Activate.ps1"
-python main.py
